e357a815eb3fd9469e2c990874dcd2113a835006
lijinglun1996 <1749553634@qq.com>
2020-03-21 22:33:39 +0800

    添加年纪云函数

--- cloudfunctions-aliyun/addGradeItem/index.js
+++ cloudfunctions-aliyun/addGradeItem/index.js
@@ -3,8 +3,8 @@
 const db = uniCloud.database()
 exports.main = async (event, context) => {
 	//event为客户端上传的参数
-	const collection = db.collection('grade_list')
-	const res = await collection.add(event)
+	const collection = db.collection('grade_list') // 获取表'grade_list'的集合对象
+	const res = await collection.add(event) //添加
 	return res.data
-	return res
+	return res // 返回json给客户端
 };
