a69bce803b5dbbf11009e2f49a18c422e04e23b7
lijinglun1996 <1749553634@qq.com>
2020-03-22 11:34:37 +0800

    增加提示语

--- cloudfunctions-aliyun/addGradeItem/index.js
+++ cloudfunctions-aliyun/addGradeItem/index.js
@@ -3,7 +3,8 @@
 const db = uniCloud.database()
 exports.main = async (event, context) => {
 	//event为客户端上传的参数
-	const collection = db.collection('grade_list') // 获取表'grade_list'的集合对象
+	//context中可获取客户端调用的上下文
+	const collection = db.collection('grade_list') // 获取表'grade_list'的集合对象引用
 	const res = await collection.add(event) //添加
 	return res.data
 	return res // 返回json给客户端
