146897eee0754dfd7712aa7de255d244f5fa46b5
lijinglun1996 <1749553634@qq.com>
2020-03-26 01:14:02 +0800

    增加绑定学校学院信息、增加返回功能、重复绑定提示语

--- pages/student_bind/student_bind.vue
+++ pages/student_bind/student_bind.vue
@@ -2,6 +2,26 @@
 <template>
     <view class="margin-lr">
         <view class="uni-list" style="margin-top: 30px;">
+			<view class="uni-list-cell">
+			    <view class="uni-list-cell-left">
+			        选择学校
+			    </view>
+			    <view class="uni-list-cell-db">
+			        <picker @change="bindPickerChange" :value="gradeIndex" :range="gradeArr" range-key="name">
+			            <view class="uni-input">{{gradeName}}</view>
+			        </picker>
+			    </view>
+			</view>
+			<view class="uni-list-cell">
+			    <view class="uni-list-cell-left">
+			        选择学院
+			    </view>
+			    <view class="uni-list-cell-db">
+			        <picker @change="bindPickerChange" :value="gradeIndex" :range="gradeArr" range-key="name">
+			            <view class="uni-input">{{gradeName}}</view>
+			        </picker>
+			    </view>
+			</view>
             <view class="uni-list-cell">
                 <view class="uni-list-cell-left">
                     选择年级
@@ -15,7 +35,7 @@
 
             <view class="uni-list-cell">
                 <view class="uni-list-cell-left">
-                    选择班级
+                    选择专业班级
                 </view>
                 <view class="uni-list-cell-db">
                     <picker @change="bindClassChange" :value="classIndex" :range="classArr" range-key="name">
@@ -37,7 +57,8 @@
         </view>
        
         <view class="buttonGroup">
-            <button type="primary" @click="bind">确定</button>
+            <button type="primary" style="margin-bottom: 30rpx;" @click="bind">确定</button>
+			<button class="linkBtn" type="primary" @click="go_back">返回</button>
         </view>
     </view>
 </template>
@@ -69,6 +90,17 @@
                 console.log(res);
                 this.classList = res.result
                 this.gradeArr = res.result.gradeList
+				if (uni.getStorageSync('class_id')) {
+					uni.showModal({
+						title:'提示',
+						content:'您已经设置过班级信息了，真的要重新绑定吗？',
+						success: (res) => {
+							if (res.cancel) {
+								this.go_back()
+							}
+						}
+					})
+				}
             })
             .catch(err => {
                 uni.hideLoading();
@@ -122,13 +154,28 @@
                     uni.hideLoading();
                     console.error(err);
                 });
-            }
+            },
+			go_back: function() {
+				if (getCurrentPages().length > 1) {
+					uni.navigateBack();
+				} else {
+					uni.switchTab({
+						url: '../index/index'
+					})
+				}
+			},
         }
-    }
+    };
 </script>
 
 <style>
 .buttonGroup{
-	margin-top: 150rpx;
+	margin-top: 80rpx;
+	/* margin-bottom: 100rpx; */
+}
+.buttonGroup .linkBtn{
+	/* margin-top: 80rpx; */
+	/* margin-bottom: 100rpx; */
+	background: linear-gradient(135deg, #576FEC 0%, #576FEC 76%, #536DEC 76%);
 }
 </style>
