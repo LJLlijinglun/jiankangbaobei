a33907b95d73ad89cf928170f1aa835a75497563
lijinglun1996 <1749553634@qq.com>
2020-04-02 21:51:20 +0800

    设置 云空间spaceId、clientSecret

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ import App from './App'
 Vue.config.productionTip = false
 
 App.mpType = 'app'
+Vue.prototype.$cloud = uniCloud.init({
+	provider:'aliyun',
+	spaceId:'03a765bd-d1cd-4209-84c0-c88b603f6fcb',
+	clientSecret:'vyQjusDQVTxP2BlgAxIVkg=='
+})
 const app = new Vue({
     ...App
 })
